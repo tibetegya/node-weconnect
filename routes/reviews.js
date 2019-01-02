291920224437631468e5286225382a36ac94306c
George Tibetegya <gtibetegya@gmail.com>
2019-01-02 18:57:50 +0300

    Revert "#162904902 add mongo db database"

--- routes/reviews.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET reviews */
-router.get('/', function(req, res, next) {
-  res.json({ title: 'Reviews' });
-});
-
-module.exports = router;
--- utils/constants.js
+++
@@ -1,5 +0,0 @@
-const dev_db_url = 'mongodb://localhost/test';
-
-module.exports = {
-  dev_db_url,
-};
